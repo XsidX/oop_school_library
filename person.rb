a31d222ea7d0f47ad865f617cdff4f3275b48fc2
Sidney Kaguli <developer.sid@outlook.com>
2022-09-27 13:08:36 +0300

    add nameable inheritance and test runs

--- person.rb
+++ person.rb
@@ -1,8 +1,13 @@
-class Person
+require_relative 'nameable'
+require_relative 'decorators'
+
+class Person < Nameable
   attr_accessor :name, :age, :parent_permission
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission = true)
+    super()
+
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -13,9 +18,20 @@ class Person
     of_age? || parent_permission
   end
 
+  def correct_name
+    name
+  end
+
   private
 
   def of_age?
     age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+p capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
