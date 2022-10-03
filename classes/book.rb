f31715b5c6d215221a8d4bf9bce694ad162394b0
Sidney Kaguli <developer.sid@outlook.com>
2022-10-03 17:08:06 +0300

    add method for adding a  new book and selecting a book for renting

--- classes/book.rb
+++ classes/book.rb
@@ -15,4 +15,43 @@ class Book
     index = Book.all.index(self)
     "#{index}) Title: \"#{title}\", Author: #{author}"
   end
+
+  private
+
+  def self.list_books
+    puts 'List of books:'
+    puts self.all.empty? ? 'No books available yet' : self.all
+  end
+
+  def self.create_book
+    puts
+    puts 'Title:'
+    title = gets.chomp
+
+    puts 'Author:'
+    author = gets.chomp
+
+    book = self.new(title, author)
+
+    puts
+    puts 'Book created successfully'
+    puts book
+  end
+
+  def self.select_book_for_renting
+    puts 'Select a book from the following list by number'
+    if self.all.empty?
+      puts
+      puts 'No books available'
+      return
+    end
+    puts self.all
+    book_idx = gets.chomp.to_i
+
+    if book_idx > self.all.length || book_idx.negative?
+      puts 'Invalid book number'
+      return
+    end
+    book_idx
+  end
 end
