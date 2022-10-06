91c1ca394dc379cb769976f51c84ae6cafca7495
Roseokpe <roseokpe@gmail.com>
2022-10-06 11:49:42 -0700

    fix linters errors

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,15 +1,12 @@
 require '../classes/classroom'
 
 describe Classroom do
-
-  classroom = Classroom.new(101)
-  classroom1 = Classroom.new(102)
+  Classroom.new(101)
+  Classroom.new(102)
 
   it 'should create a classroom' do
-
     expect(Classroom.all.length).to eql(2)
     expect(Classroom.all[0].label).to eql(102)
     expect(Classroom.all[1].label).to eql(101)
-
   end
 end
